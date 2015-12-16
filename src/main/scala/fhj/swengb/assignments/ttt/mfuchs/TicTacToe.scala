aa8de8f5d8f5dcd64153c95e26702339b20138f3
Michael Fuchs <admin@dekilla.at>
2015-12-16 23:25:18 +0100

    soviel ... unfassbar .. und es funktioniert ... also ... 2 Spieler halt nur aber trotzdem ... sehr fein

--- src/main/scala/fhj/swengb/assignments/ttt/mfuchs/TicTacToe.scala
+++ src/main/scala/fhj/swengb/assignments/ttt/mfuchs/TicTacToe.scala
@@ -186,7 +186,7 @@ case class TicTacToe(moveHistory: Map[TMove, Player],
     * The game is over if either of a player wins or there is a draw.
     */
   val gameOver : Boolean = {
-    if (moveHistory.size >= 5 && winner != None) {true} else {false}
+    if (winner.isDefined || moveHistory.size == 9) {println("Spiel vorbei, " + winner.head._1 + " hat gewonnen"); true} else {false}
   }
 
   /**
@@ -213,93 +213,6 @@ case class TicTacToe(moveHistory: Map[TMove, Player],
     */
   def winner: Option[(Player, Set[TMove])] = {
 
-  /*
-    val a1:Option[TMove] = if (moveHistory.contains(TopLeft)) Some(TopLeft) else None
-    val a2:Option[TMove] = if (moveHistory.contains(TopCenter)) Some(TopCenter) else None
-    val a3:Option[TMove] = if (moveHistory.contains(TopRight)) Some(TopRight) else None
-
-    val b1:Option[TMove] = if (moveHistory.contains(MiddleLeft)) Some(MiddleLeft) else None
-    val b2:Option[TMove] = if (moveHistory.contains(MiddleCenter)) Some(MiddleCenter) else None
-    val b3:Option[TMove] = if (moveHistory.contains(MiddleRight)) Some(MiddleRight) else None
-
-    val c1:Option[TMove] = if (moveHistory.contains(BottomLeft)) Some(BottomLeft) else None
-    val c2:Option[TMove] = if (moveHistory.contains(BottomCenter)) Some(BottomCenter) else None
-    val c3:Option[TMove] = if (moveHistory.contains(BottomRight)) Some(BottomRight) else None
-*/
-
-    /*
-     val a1 = Option(moveHistory(TopLeft)) match {
-       case Some(value) => value
-       case None => None
-     }
-
-     val a2 = Option(moveHistory(TopCenter)) match {
-       case Some(value) => value
-       case None => None
-     }
-
-     val a3 = Option(moveHistory(TopRight)) match {
-       case Some(value) => value
-       case None => None
-     }
-
-     val b1 = Option(moveHistory(MiddleLeft)) match {
-       case Some(value) => value
-       case None => None
-     }
-
-     val b2 = Option(moveHistory(MiddleCenter)) match {
-       case Some(value) => value
-       case None => None
-     }
-
-     val b3 = Option(moveHistory(MiddleRight)) match {
-       case Some(value) => value
-       case None => None
-     }
-
-     val c1 = Option(moveHistory(BottomLeft)) match {
-       case Some(value) => value
-       case None => None
-     }
-
-     val c2 = Option(moveHistory(BottomCenter)) match {
-       case Some(value) => value
-       case None => None
-     }
-
-     val c3 = Option(moveHistory(BottomRight)) match {
-       case Some(value) => value
-       case None => None
-     }
-
-    */
-/*
-     val g1:List[Option[TMove]] = List(a1, a2, a3)
-     val g2:List[Option[TMove]] = List(b1, b2, b3)
-     val g3:List[Option[TMove]] = List(c1, c2, c3)
-     val g4:List[Option[TMove]] = List(a1, b1, c1)
-     val g5:List[Option[TMove]] = List(a2, b2, c2)
-     val g6:List[Option[TMove]] = List(a3, b3, c3)
-     val g7:List[Option[TMove]] = List(a1, b2, c3)
-     val g8:List[Option[TMove]] = List(a3, b2, c1)
-*/
-    /*
-     def detWinner(): Option[Player] = {
-       val winninggames = List(g1, g2, g3, g4, g5, g6, g7, g8)
-       for (game <- winninggames) {
-         val vwinner = if (game._1 == game._2 && game._2 == game._3 && game._1 != None && game._2 != None && game._3 != None) {
-           game._1
-         } else None
-         if (vwinner != None) {
-           scala.util.control.Breaks.break()
-         }
-         vwinner
-       }
-     }
-
-*/
-
     val stepsA = moveHistory.filter(_._2 == PlayerA).keySet
     val stepsB = moveHistory.filter(_._2 == PlayerB).keySet
 
@@ -313,22 +226,17 @@ case class TicTacToe(moveHistory: Map[TMove, Player],
       (TopLeft,MiddleCenter,BottomRight),
       (TopRight,MiddleCenter,BottomLeft))
 
-      for (game <- winninggames) {
-        if (stepsA.contains(game._1) && stepsA.contains(game._2) && stepsA.contains(game._3)) {var winner = PlayerA}
-        else if (stepsB.contains(game._1) && stepsB.contains(game._2) && stepsB.contains(game._3)) {var winner = PlayerB}
-      }
+    var winner = ""
 
-    if (winner == PlayerA) Some(PlayerA, stepsA)
-    else if (winner == PlayerB) Some(PlayerB, stepsB)
+    for (game <- winninggames) {
+      if (stepsA.contains(game._1) && stepsA.contains(game._2) && stepsA.contains(game._3)) {winner = "A"}//; scala.util.control.Breaks.break()}
+      else if (stepsB.contains(game._1) && stepsB.contains(game._2) && stepsB.contains(game._3)) {winner = "B"}//; scala.util.control.Breaks.break()}
+    }
+
+    if (winner == "A") Some(PlayerA, stepsA)
+    else if (winner == "B") Some(PlayerB, stepsB)
     else None
 
-    /*
-    winner match {
-      case PlayerA => Some(PlayerA, stepsA)
-      case PlayerB => Some(PlayerB, stepsB)
-      case _ => None
-    }
-   */
   }
 
   /**
@@ -340,8 +248,8 @@ case class TicTacToe(moveHistory: Map[TMove, Player],
     */
   def turn(p: TMove, player: Player): TicTacToe = {
     player match {
-      case PlayerA => println("next Player is Player B"); TicTacToe(moveHistory + (p -> player),PlayerB)
-      case PlayerB => println("next Player is Player A"); TicTacToe(moveHistory + (p -> player),PlayerA)
+      case PlayerA => TicTacToe(moveHistory + (p -> player),PlayerB)
+      case PlayerB => TicTacToe(moveHistory + (p -> player),PlayerA)
     }
   }
 }
--- src/main/scala/fhj/swengb/assignments/ttt/mfuchs/TicTacToeApp.scala
+++ src/main/scala/fhj/swengb/assignments/ttt/mfuchs/TicTacToeApp.scala
@@ -1,8 +1,10 @@
 package fhj.swengb.assignments.ttt.mfuchs
 
 import javafx.application.Application
+import javafx.event.EventHandler
 import javafx.fxml.{FXML, FXMLLoader}
-import javafx.scene.control.Button
+import javafx.scene.control.{Label, Button}
+import javafx.scene.input.MouseEvent
 import javafx.scene.{Scene, Parent}
 import javafx.stage.Stage
 
@@ -29,8 +31,9 @@ class TicTacToeApp extends Application {
     loader.load[Parent]() // side effect
     val scene = new Scene(loader.getRoot[Parent])
     stage.setScene(scene)
-    stage.getScene.getStylesheets.add(Css)
+    scene.getStylesheets().add(Css)
     stage.show()
+
 
   } catch {
     case NonFatal(e) => {
@@ -50,66 +53,211 @@ class TicTacToeAppController {
   @FXML private var btn_C1: Button = _
   @FXML private var btn_C2: Button = _
   @FXML private var btn_C3: Button = _
+  @FXML private var btn_restart: Button = _
+  @FXML private var lbl_winner: Label = _
 
+  val buttons:List[Button] = List(btn_A1,btn_A2,btn_A3,btn_B1,btn_B2,btn_B3,btn_C1,btn_C2,btn_C3)
 
   def initialize(): Unit = {
 
   }
 
+  def restart(): Unit = {
+    game = TicTacToe()
+    btn_A1.setText("")
+    btn_A2.setText("")
+    btn_A3.setText("")
+    btn_B1.setText("")
+    btn_B2.setText("")
+    btn_B3.setText("")
+    btn_C1.setText("")
+    btn_C2.setText("")
+    btn_C3.setText("")
+    btn_A1.setDisable(false)
+    btn_A2.setDisable(false)
+    btn_A3.setDisable(false)
+    btn_B1.setDisable(false)
+    btn_B2.setDisable(false)
+    btn_B3.setDisable(false)
+    btn_C1.setDisable(false)
+    btn_C2.setDisable(false)
+    btn_C3.setDisable(false)
+    lbl_winner.setText("")
+  }
+
+
   var game = TicTacToe()
 
   def A1(): Unit = {
-    //btn_A1.setText("X")
+    btn_A1.setText(if (game.moveHistory.size % 2 == 0) "x" else "o")
     game = game.turn(TopLeft, game.nextPlayer)
-    println(game.asString())
+    //println(game.asString())
+    btn_A1.setDisable(true)
+    if (game.gameOver && game.winner.isDefined) {
+      lbl_winner.setText(game.winner.head._1.toString + " hat gewonnen")
+      btn_A1.setDisable(true)
+      btn_A2.setDisable(true)
+      btn_A3.setDisable(true)
+      btn_B1.setDisable(true)
+      btn_B2.setDisable(true)
+      btn_B3.setDisable(true)
+      btn_C1.setDisable(true)
+      btn_C2.setDisable(true)
+      btn_C3.setDisable(true)
+    }
   }
 
   def A2(): Unit = {
-    //btn_A2.setText("X")
+    btn_A2.setText(if (game.moveHistory.size % 2 == 0) "x" else "o")
     game = game.turn(TopCenter, game.nextPlayer)
-    println(game.asString())
+    //println(game.asString())
+    btn_A2.setDisable(true)
+    if (game.gameOver && game.winner.isDefined) {
+      lbl_winner.setText(game.winner.head._1.toString + " hat gewonnen")
+      btn_A1.setDisable(true)
+      btn_A2.setDisable(true)
+      btn_A3.setDisable(true)
+      btn_B1.setDisable(true)
+      btn_B2.setDisable(true)
+      btn_B3.setDisable(true)
+      btn_C1.setDisable(true)
+      btn_C2.setDisable(true)
+      btn_C3.setDisable(true)
+    }
   }
 
   def A3(): Unit = {
-    //btn_A3.setText("X")
+    btn_A3.setText(if (game.moveHistory.size % 2 == 0) "x" else "o")
     game = game.turn(TopRight, game.nextPlayer)
-    println(game.asString())
+    //println(game.asString())
+    btn_A3.setDisable(true)
+    if (game.gameOver && game.winner.isDefined) {
+      lbl_winner.setText(game.winner.head._1.toString + " hat gewonnen")
+      btn_A1.setDisable(true)
+      btn_A2.setDisable(true)
+      btn_A3.setDisable(true)
+      btn_B1.setDisable(true)
+      btn_B2.setDisable(true)
+      btn_B3.setDisable(true)
+      btn_C1.setDisable(true)
+      btn_C2.setDisable(true)
+      btn_C3.setDisable(true)
+    }
   }
 
   def B1(): Unit = {
-    //btn_B1.setText("X")
+    btn_B1.setText(if (game.moveHistory.size % 2 == 0) "x" else "o")
     game = game.turn(MiddleLeft, game.nextPlayer)
-    println(game.asString())
+    //println(game.asString())
+    btn_B1.setDisable(true)
+    if (game.gameOver) buttons foreach(_.setDisable(true))
+    if (game.gameOver && game.winner.isDefined) {
+      lbl_winner.setText(game.winner.head._1.toString + " hat gewonnen")
+      btn_A1.setDisable(true)
+      btn_A2.setDisable(true)
+      btn_A3.setDisable(true)
+      btn_B1.setDisable(true)
+      btn_B2.setDisable(true)
+      btn_B3.setDisable(true)
+      btn_C1.setDisable(true)
+      btn_C2.setDisable(true)
+      btn_C3.setDisable(true)
+    }
   }
 
   def B2(): Unit = {
-    //btn_B2.setText("X")
+    btn_B2.setText(if (game.moveHistory.size % 2 == 0) "x" else "o")
     game = game.turn(MiddleCenter, game.nextPlayer)
-    println(game.asString())
+    //println(game.asString())
+    btn_B2.setDisable(true)
+    if (game.gameOver && game.winner.isDefined) {
+      lbl_winner.setText(game.winner.head._1.toString + " hat gewonnen")
+      btn_A1.setDisable(true)
+      btn_A2.setDisable(true)
+      btn_A3.setDisable(true)
+      btn_B1.setDisable(true)
+      btn_B2.setDisable(true)
+      btn_B3.setDisable(true)
+      btn_C1.setDisable(true)
+      btn_C2.setDisable(true)
+      btn_C3.setDisable(true)
+    }
   }
 
   def B3(): Unit = {
-    //btn_B3.setText("X")
+    btn_B3.setText(if (game.moveHistory.size % 2 == 0) "x" else "o")
     game = game.turn(MiddleRight, game.nextPlayer)
-    println(game.asString())
+    //println(game.asString())
+    btn_B3.setDisable(true)
+    if (game.gameOver && game.winner.isDefined) {
+      lbl_winner.setText(game.winner.head._1.toString + " hat gewonnen")
+      btn_A1.setDisable(true)
+      btn_A2.setDisable(true)
+      btn_A3.setDisable(true)
+      btn_B1.setDisable(true)
+      btn_B2.setDisable(true)
+      btn_B3.setDisable(true)
+      btn_C1.setDisable(true)
+      btn_C2.setDisable(true)
+      btn_C3.setDisable(true)
+    }
   }
 
   def C1(): Unit = {
-    //btn_C1.setText("X")
+    btn_C1.setText(if (game.moveHistory.size % 2 == 0) "x" else "o")
     game = game.turn(BottomLeft, game.nextPlayer)
-    println(game.asString())
+    //println(game.asString())
+    btn_C1.setDisable(true)
+    if (game.gameOver && game.winner.isDefined) {
+      lbl_winner.setText(game.winner.head._1.toString + " hat gewonnen")
+      btn_A1.setDisable(true)
+      btn_A2.setDisable(true)
+      btn_A3.setDisable(true)
+      btn_B1.setDisable(true)
+      btn_B2.setDisable(true)
+      btn_B3.setDisable(true)
+      btn_C1.setDisable(true)
+      btn_C2.setDisable(true)
+      btn_C3.setDisable(true)
+    }
   }
 
   def C2(): Unit = {
-    //btn_C2.setText
+    btn_C2.setText(if (game.moveHistory.size % 2 == 0) "x" else "o")
     game = game.turn(BottomCenter, game.nextPlayer)
-    println(game.asString())
+    //println(game.asString())
+    btn_C2.setDisable(true)
+    if (game.gameOver && game.winner.isDefined) {
+      lbl_winner.setText(game.winner.head._1.toString + " hat gewonnen")
+      btn_A1.setDisable(true)
+      btn_A2.setDisable(true)
+      btn_A3.setDisable(true)
+      btn_B1.setDisable(true)
+      btn_B2.setDisable(true)
+      btn_B3.setDisable(true)
+      btn_C1.setDisable(true)
+      btn_C2.setDisable(true)
+      btn_C3.setDisable(true)
+    }
   }
 
   def C3(): Unit = {
-    //btn_C3.setText("X")
+    btn_C3.setText(if (game.moveHistory.size % 2 == 0) "x" else "o")
     game = game.turn(BottomRight, game.nextPlayer)
-    println(game.asString())
+    //println(game.asString())
+    btn_C3.setDisable(true)
+    if (game.gameOver && game.winner.isDefined) {
+      lbl_winner.setText(game.winner.head._1.toString + " hat gewonnen")
+      btn_A1.setDisable(true)
+      btn_A2.setDisable(true)
+      btn_A3.setDisable(true)
+      btn_B1.setDisable(true)
+      btn_B2.setDisable(true)
+      btn_B3.setDisable(true)
+      btn_C1.setDisable(true)
+      btn_C2.setDisable(true)
+      btn_C3.setDisable(true)
+    }
   }
 
 }
